dfab56517658dbafc23a5e2176763b27693a296b
Ven Popov <vencislav.popov@gmail.com>
2024-05-11 00:29:12 +0200

    add basic data preprocsessing

--- R/functions.R
+++ R/functions.R
@@ -15,3 +15,32 @@ extract_object_from_rdata <- function(path, object_name = "data_an") {
   }
   env[[object_name]]
 }
+
+#' Preprocesses the data
+#'
+#' This function preprocesses the input data by converting the trial column to numeric,
+#' transforming the gap column values, and categorizing the itemtype based on serpos values.
+#'
+#' @param data The input data frame
+#'
+#' @return The preprocessed data frame
+#'
+#' @examples
+preprocess_data <- function(data) {
+  mutate <- dplyr::mutate
+  case_when <- dplyr::case_when
+
+  data |> 
+    mutate(trial = as.numeric(trial),
+           gap = ifelse(gap == "short", "500 ms", "3000 ms"),
+           itemtype = case_when(
+             serpos %in% 1:3 ~ "SP1-3",
+             serpos %in% 4:6 ~ "SP4-6",
+             serpos %in% 7:9 ~ "SP7-9"
+           ))
+}
+
+get_data <- function(path) {
+  data <- extract_object_from_rdata(path)
+  preprocess_data(data)
+}
--- _targets.R
+++ _targets.R
@@ -5,9 +5,9 @@
 library(targets)
 
 # Set target options:
-tar_option_set(
-  packages = c("tibble") 
-)
+# tar_option_set(
+#   packages = c("dplyr") 
+# )
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
@@ -15,6 +15,6 @@ tar_source()
 # Pipeline
 list(
   tar_target(data_files, c("data-raw/Exp1.Rdata", "data-raw/Exp2.Rdata"), format = "file"),
-  tar_target(exp1_data, extract_object_from_rdata(data_files[1])),
-  tar_target(exp2_data, extract_object_from_rdata(data_files[2]))
+  tar_target(exp1_data, get_data(data_files[1])),
+  tar_target(exp2_data, get_data(data_files[2]))
 )
